6c43229c8a4d66f1f8526a292afabb5cce69201a
grovernitin1 <grovernitin1@users.noreply.github.com>
2015-04-17 00:12:17 -0500

    Update GoogleTest.java

--- src/GoogleTest.java
+++ src/GoogleTest.java
@@ -2,7 +2,7 @@ import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+// import static org.hamcrest.CoreMatchers.*; 
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
